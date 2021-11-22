d86f1a5dc66ba88eca1e63edae20eb7f6ccbe88d
jpskgc <jpskgc.v3@gmail.com>
2021-11-22 23:06:20 +0900

    initial commit

--- .storybook/config.js
+++
@@ -1,8 +0,0 @@
-import { configure } from '@storybook/vue';
-
-function loadStories() {
-  const req = require.context('../components', true, /\.stories\.ts$/);
-  req.keys().forEach(filename => req(filename));
-}
-
-configure(loadStories, module);

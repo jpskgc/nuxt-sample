d258473f9cdf2878278543f2089557633ace41f7
jpskgc <jpskgc.v3@gmail.com>
2021-12-01 03:07:25 +0900

    add registration page

---
+++ components/registration-form/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <form>
+    <v-text-field
+      v-model="name"
+      label="Name"
+      required
+    ></v-text-field>
+    <v-text-field
+      v-model="email"
+      label="E-mail"
+      required
+    ></v-text-field>
+
+    <v-btn
+      class="mr-4"
+      @click="submit"
+    >
+      submit
+    </v-btn>
+    <v-btn @click="clear">
+      clear
+    </v-btn>
+  </form>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+
+@Component({})
+export default class RegistrationFormComponent extends Vue {
+  
+}
+</script>
---
+++ pages/register/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <RegistrationFormComponent />
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+import RegistrationFormComponent from '~/components/registration-form/index.vue'
+
+@Component({
+  components: {
+    RegistrationFormComponent
+  }
+})
+export default class Register extends Vue {}
+</script>

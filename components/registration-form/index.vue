1742748110f87cb5af00e64a5cc102e72491a2b3
jpskgc <jpskgc.v3@gmail.com>
2021-12-02 03:03:02 +0900

    change pages

--- components/registration-form/index.vue
+++ components/registration-form/index.vue
@@ -5,12 +5,47 @@
       label="Name"
       required
     ></v-text-field>
-    <v-text-field
-      v-model="email"
-      label="E-mail"
-      required
-    ></v-text-field>
-
+<v-menu
+        ref="menu"
+        v-model="menu"
+        :close-on-content-click="false"
+        :return-value.sync="date"
+        transition="scale-transition"
+        offset-y
+        min-width="auto"
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-text-field
+            v-model="date"
+            label="Picker in menu"
+            prepend-icon="mdi-calendar"
+            readonly
+            v-bind="attrs"
+            v-on="on"
+          ></v-text-field>
+        </template>
+        <v-date-picker
+          v-model="date"
+          no-title
+          scrollable
+        >
+          <v-spacer></v-spacer>
+          <v-btn
+            text
+            color="primary"
+            @click="menu = false"
+          >
+            Cancel
+          </v-btn>
+          <v-btn
+            text
+            color="primary"
+            @click="$refs.menu.save(date)"
+          >
+            OK
+          </v-btn>
+        </v-date-picker>
+      </v-menu>
     <v-btn
       class="mr-4"
       @click="submit"
@@ -28,6 +63,9 @@ import { Component, Vue } from 'nuxt-property-decorator'
 
 @Component({})
 export default class RegistrationFormComponent extends Vue {
-  
+
+  get date(): string {
+    return (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10)
+  }
 }
 </script>
--- pages/check/index.vue
+++
@@ -1,9 +0,0 @@
-<template>
-</template>
-
-<script lang="ts">
-import { Component, Vue } from 'nuxt-property-decorator'
-
-@Component({})
-export default class Check extends Vue {}
-</script>
